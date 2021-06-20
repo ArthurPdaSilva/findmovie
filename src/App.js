7a21ad2c59b67e9b1d7868f439e2fdaa13a1269a
Arthur Pereira da Silva <arthurpdasillva@gmail.com>
2021-06-20 16:21:27 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Routes from './routes';
 import MovieProvider from './contexts/movie';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';   
+import 'react-toastify/dist/ReactToastify.css'; 
 
 export default function App() {
   return (
